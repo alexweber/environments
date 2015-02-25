0ce5489c8967ad043006713741ca34421cd12ad6
Alex Weber <alexweber15@gmail.com>
2015-02-25 17:23:06 -0300

    fix bug in banner template

--- templates/banner.tpl.php
+++ templates/banner.tpl.php
@@ -1,4 +1,4 @@
-<div id="environments-notify" data-alert class="alert-box <?php print $env; ?> <?php print $environment_alias; ?>">
+<div id="environments-notify" data-alert class="alert-box <?php print $environment; ?> <?php print $environment_alias; ?>">
   <div class="message"><?php print $environment_message; ?></div>
   <a href="#" class="close">×</a>
 </div>
