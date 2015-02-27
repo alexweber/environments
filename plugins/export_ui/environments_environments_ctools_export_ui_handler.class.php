f6779a2415f136bc04467b1ec61e4168128294b7
Alex Weber <alexweber15@gmail.com>
2015-02-27 11:45:47 -0300

    further work on bundles groundwork

--- plugins/export_ui/environments_environments_ctools_export_ui_handler.class.php
+++ plugins/export_ui/environments_environments_ctools_export_ui_handler.class.php
@@ -5,7 +5,26 @@
  * Contains environments_ctools_export_ui_handler.
  */
 
-class environments_environments_ctools_export_ui_handler extends environments_ctools_export_ui_handler {
+class environments_environments_ctools_export_ui_handler extends ctools_export_ui {
+
+	/**
+	 * @inheritdoc
+	 */
+	function build_operations($item) {
+		$allowed_operations = parent::build_operations($item);
+
+		// Move our custom "Manage Tasks" operation to the bottom of the list.
+		$task = array_key_exists('tasks', $allowed_operations)
+			? $allowed_operations['tasks']
+			: NULL;
+
+		if ($task) {
+			unset ($allowed_operations['tasks']);
+			$allowed_operations['tasks'] = $task;
+		}
+
+		return $allowed_operations;
+	}
 
   /**
    * @inheritdoc
