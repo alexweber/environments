d9be5a4f4887ecdb028e6f878b635b4aad3fac9e
Alex Weber <alexweber15@gmail.com>
2015-03-06 16:50:00 -0300

    replace bundler + compass with gulp + libsass for our styles & throw in some js linting just cause

---
+++ assets/Gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
+var scsslint = require('gulp-scss-lint');
+
+gulp.task('sass', function () {
+	gulp.src('./scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('lintscss', function() {
+	gulp.src('./scss/*.scss')
+		.pipe(scsslint())
+		.pipe(scsslint.failReporter());
+});
+
+gulp.task('lintjs', function () {
+	return gulp.src('./js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default', {verbose: true}));
+});
+
+gulp.task('default', ['lintjs', 'sass']);
